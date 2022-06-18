e60c873417422971914148089fdbeb52a97cdbd9
Pamush <104290993+Pamush@users.noreply.github.com>
2022-06-18 23:27:08 +0530

    Create outputs.py

---
+++ outputs.py
@@ -0,0 +1,137 @@
+#Q1
+
+Enter the string to be reversed Amush
+hsumA 
+
+Process finished with exit code 0
+
+#Q2
+
+Enter starting of Range
+5
+Enter end of Range
+105
+Enter the number
+7
+Numbers in range  5  to  105  divisible by  7  are:
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+77
+84
+91
+98
+
+
+
+#Q3
+
+Enter side A
+5
+Enter side B
+3
+Enter side C
+4
+Area of the triangle is:  6.0
+
+  
+#Q4
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+
+#Q5
+
+Enter the no of rows: 10
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYZAB
+CDEFGHIJ
+KLMNOPQRS
+TUVWXYZABC
+
+#Q6
+
+Please, Enter the Lowest Range Value : 
+1
+Please, Enter the Upper Range Value : 
+10
+
+The Prime Numbers in the range are: 
+1 ) 2
+2 ) 3
+3 ) 5
+4 ) 7
+
+#Q7
+
+77
+154
+231
+308
+385
+462
+
+#Q8
+
+Enter 1th number: 10
+Enter 2th number: 20
+Enter 3th number: 30
+Enter 4th number: 40
+Enter 5th number: 50
+Enter 6th number: 60
+Enter 7th number: 70
+Enter 8th number: 80
+Enter 9th number: 90
+Enter 10th number: 100
+[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+Positive numbers are: 
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+Negative numbers are: 
+Odd numbers are: 
+Even numbers are: 
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+No of times each number occurs in the List:
+{10: 1, 20: 1, 30: 1, 40: 1, 50: 1, 60: 1, 70: 1, 80: 1, 90: 1, 100: 1}
+
+#Q9
+
+Enter a string: I love music
+{'I': 1, 'love': 1, 'music': 1}
